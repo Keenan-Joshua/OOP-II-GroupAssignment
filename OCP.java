76c6918ba13be8376244909ce24922305f245b5d
Abraham Konchellah <abrahamkonchellah@gmail.com>
2025-06-10 13:05:11 +0300

    Update OCP.java

--- OCP.java
+++ OCP.java
@@ -1,28 +1,30 @@
 //Does not adhere to Open/Closed principle
-//The getDiscount Method is not closed for modification
-public class Discount {
-    public double getDiscount(String customerType) {
-        if (customerType.equals("regular")) return 0.1;
-        else if (customerType.equals("vip")) return 0.2;
-        else return 0;
+//The getShippingCost Method is not closed for modification
+public class ShippingCalculator {
+    public double getShippingCost(String shippingType) {
+        if (shippingType.equals("standard")) return 5.99;
+        else if (shippingType.equals("express")) return 12.99;
+        else return 0.0; // free shipping
+    }
+}
     }
 }
 
 //Adhere to Open/closed principle
 // The program is open to extension by creation of new classes 
 // closed to modification : no need to modify existing classes
-public interface Discount {
-    double getDiscount();
+public interface ShippingCalculator {
+    double getShippingCost();
 }
 
-public class RegularDiscount implements Discount {
-    public double getDiscount() {
-        return 0.1;
+public class Standard implements ShippingCalculator {
+    public double getShippingCost() {
+        return 5.99;
     }
 }
 
-public class VIPDiscount implements Discount {
-    public double getDiscount() {
-        return 0.2;
+public class Express implements ShippingCalculator {
+    public double getShippingCost() {
+        return 12.99;
     }
 }
